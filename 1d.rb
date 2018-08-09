5f62294f7f693754a084d4e2c66d90df0ee41e44
Taylor Otey <taylorotey5@gmail.com>
2018-08-09 17:41:07 +0000

    automatically backed up by learn

---
+++ 1d.rb
@@ -0,0 +1,18 @@
+one_direction = {"Zayn" => 25,
+                "Liam" => 24,
+                "Harry" => 24,
+                "Nial" => 24
+}
+
+#How can you print out all the members of 1D and their ages like the following:
+#Zayn is 25 years 
+
+one_direction.each do |name, age|
+  
+end
+
+#How can 
+
+
+
+
